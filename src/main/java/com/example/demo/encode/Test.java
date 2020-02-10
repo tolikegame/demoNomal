6f2325bde7c03c582176b8a43eb754b38a9302ad
Larry <tgfc045@tgfc.tw>
2020-02-10 09:39:14 +0800

    新增加密格式 {bcrypt}$2a$10$UOKAAUwJHp8SWEh.vMjwYeDXy/5ODQF21s.5UAHzF7kSJ8ZBg5jny

--- src/main/java/com/example/demo/encode/Test.java
+++ src/main/java/com/example/demo/encode/Test.java
@@ -3,6 +3,8 @@ package com.example.demo.encode;
 //import org.apache.commons.codec.binary.Base32;
 import org.jboss.aerogear.security.otp.api.Base32;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.io.UnsupportedEncodingException;
 import java.util.Base64;
@@ -14,15 +16,21 @@ public class Test {
 //        System.out.println(base32.encodeAsString("8nz@^W3YP3UKE#ecKFLw".getBytes()));
 
         BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
-        String psw = "!qazzaq1";
+        String psw = "tgfc045";
         String encode = passwordEncoder.encode(psw);
         System.out.println("BCrypt==>"+encode);
+
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+        String password = "tgfc045";
+        String encoder2 = encoder.encode(password);
+        System.out.println("BCrypt==>"+encoder2);
+
 
         String secret = Base32.random();
         System.out.println("Base32==>"+secret);
 
-        final Base64.Decoder decoder = Base64.getDecoder();
-        final Base64.Encoder encoder = Base64.getEncoder();
+        final Base64.Decoder decoder64 = Base64.getDecoder();
+        final Base64.Encoder encoder64 = Base64.getEncoder();
         final String text = "字串文字";
         byte[] textByte = new byte[0];
         try {
@@ -31,11 +39,11 @@ public class Test {
             e.printStackTrace();
         }
 //編碼
-        final String encodedText = encoder.encodeToString(textByte);
+        final String encodedText = encoder64.encodeToString(textByte);
         System.out.println("Base64==>"+encodedText);
 //解碼
         try {
-            System.out.println("Base64解碼==>"+new String(decoder.decode(encodedText), "UTF-8"));
+            System.out.println("Base64解碼==>"+new String(decoder64.decode(encodedText), "UTF-8"));
         } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
